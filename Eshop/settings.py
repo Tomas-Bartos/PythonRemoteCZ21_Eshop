69616d352550e0c38a230d57c94cb353c9ee10d8
Tomáš <tomasbartos@email.cz>
2024-09-26 00:31:07 +0200

    Feat: create pages: base, cart, homepage, login. base.htm got some basic HTML and functional menu.
    Feat: Connect Bootstrap
    
    Feat: Connect Sass and local css
    
    Feat: Create urls.py in app
    
    Feat: Add link to new urls.py to the urls.py in Eshop folder
    
    Feat: Create views for new pages
    
    Feat: Create URLS for new pages
    
    Feat: Connect URLS in the nav menu
    
    Feat: Created staticfiles folder link in settings.py
    
    Feat: Added staticfiles to the Gitignore.
    
    Docs: Added basic commit description to the README.md

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Eshop_app',
+    'sass_processor',
 ]
 
 MIDDLEWARE = [
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Eshop.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -80,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -100,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -112,7 +109,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -122,3 +118,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SASS integration
+# Where will be CSS generated from SASS
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
+
+# Add path to static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Add default path for SASS files
+# SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
--- README.md
+++ README.md
@@ -35,5 +35,35 @@ Final Python projekt - E-shop
 -[ ] Košík
 -[ ] Zpracování objednávek - formulář, fakturace
 -[ ] Počasí
--[ ] Testing 
+-[ ] Testing
+
+<br>
+<hr>
+
+## Konvence commit zpráv:
+Feat: Když přidáš novou funkci nebo část kódu.
+- Příklad: `Feat: added user authentication`
+
+Fix: Když opravíš chybu.
+- Příklad: `Fix: corrected login redirect issue`
+
+Docs: Pro změny v dokumentaci.
+- Příklad: `Docs: updated API usage in README`
+
+Style: Pro změny související se stylem (bez změny funkcionality), např. formátování, mezery.
+- Příklad: `Style: formatted HTML templates`
+
+Refactor: Když přepíšeš kód, aniž bys změnil funkcionalitu (zlepšení struktury kódu).
+- Příklad: `Refactor: optimized query handling`
+
+Test: Pro přidání nebo úpravu testů.
+- Příklad: `Test: added unit tests for user model`
+
+Chore: Pro úpravy, které nesouvisí přímo s kódem aplikace (např. aktualizace závislostí, konfigurace).
+- Příklad: `Chore: updated dependencies`
+
+Perf: Když provedeš změny, které zlepšují výkon.
+- Příklad: `Perf: optimized image loading`
+
+<br>
 <hr>
