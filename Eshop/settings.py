da62f87425a34bb29bf791d1a91769078d8404dc
Tomáš <tomasbartos@email.cz>
2024-09-29 11:03:31 +0200

    Feat: Adding products

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -132,3 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Add default path for SASS files
 # SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
+
+# dynamic routs for images
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- Eshop_app/urls.py
+++ Eshop_app/urls.py
@@ -1,6 +1,8 @@
 # views must be mapped to urls
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 # URL configurations. To display any URL on the running server, its address must start with Eshop_app
 # for example http://127.0.0.1:8000/Eshop_app/base/
@@ -10,4 +12,4 @@ urlpatterns = [
     path('homepage/', views.home_page, name='homepage'),
     path('cart/', views.cart, name='cart'),
     path('login/', views.login, name='login'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
