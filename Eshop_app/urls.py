a28c504bf769d642fadce0b793b5d6512aace811
matlyj <bruzek.t1@gmail.com>
2024-09-30 20:40:46 +0200

    CRUD kategorie

--- Eshop_app/urls.py
+++ Eshop_app/urls.py
@@ -1,12 +1,23 @@
 # views must be mapped to urls
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from logging import getLogger
+from django.views.generic import ListView, UpdateView
 
 # URL configurations. To display any URL on the running server, its address must start with Eshop_app
-# for example http://127.0.0.1:8000/Eshop_app/base/
+# for example http://127.0.0.1:8000/Eshop_app/homepage/
 urlpatterns = [
-    path('base/', views.base),
-    path('homepage/', views.home_page, name='homepage'),
+    path('homepage/', views.homepage, name='homepage'),
     path('cart/', views.cart, name='cart'),
     path('login/', views.login, name='login'),
-]
+    path('maso/', views.maso, name='maso'),
+    path('ovoce/', views.ovoce, name='ovoce'),
+    path('mlecne-a-chlazene/', views.mlecne_a_chlazene, name='mlecne-a-chlazene'),
+    path('mrazene/', views.mrazene, name='mrazene'),
+    path('categories/', views.category_list, name='category_list'),
+    path('categories/new/', views.category_create, name='category_create'),
+    path('categories/<int:pk>/edit/', views.category_update, name='category_update'),
+    path('categories/<int:pk>/delete/', views.category_delete, name='category_delete'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Eshop_app/views.py
+++ Eshop_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product, Category
 from django.http import HttpResponse
+from .forms import CategoryForm
 
 # Create your views here.
 # request -> response
@@ -56,3 +57,36 @@ def ovoce(request):
 # mrazene
 def mrazene(request):
     return render(request, 'mrazene.html')
+
+def category_create(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm()
+    return render(request, 'category_form.html', {'form': form})
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'catgory_list.html', {'categories': categories})
+
+def category_update(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'category_form.html', {'form': form})
+
+def category_delete(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('category_list')
+    return render(request, 'category_confirm_delete.html', {'category': category})
+
